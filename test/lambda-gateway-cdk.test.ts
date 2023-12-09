6851c28e588b019756d1cd9558db1cf350f10402
gfteix <gabriel.fteixeira13@gmail.com>
2023-12-09 14:12:53 -0300

    Initial commit

---
+++ test/lambda-gateway-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaGatewayCdk from '../lib/lambda-gateway-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-gateway-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaGatewayCdk.LambdaGatewayCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
